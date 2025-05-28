332bd551e32d32549b90e17f330beeca0c652bfb
Satheesh Revoori <104457018+satheeshrevoori@users.noreply.github.com>
2025-05-28 16:36:36 +0530

    update changes

--- Personalgreeting.jsx
+++ Personalgreeting.jsx
@@ -1,16 +1,31 @@
 import React, { useState } from 'react'
 
 function Personalgreeting() {
-    const[uname, setUname] = useState("")
+  const [inputValue, setInputValue] = useState("")
+
+  function handleChange(e){
+
+    setInputValue(e.target.value)
+
+  }
+
   return (
-    <>
-    <label htmlFor='name'>Name : </label>
-    <input type = "text" placeholder='enter name' onChange={(e)=>setUname(e.target.value)}/>
-    {
-       (uname.length > 0) && <p> Hello, {uname}!</p>
-    } 
-    </>
+
+    <div>
+
+        {/* Do not remove the main div */}
+
+      <p>Enter your name:</p>
+
+      <input type="text" placeholder="Enter your name.." value={inputValue} onChange={(e) => handleChange(e)}/>
+
+      {inputValue.trim() && <p>{`Hello ${inputValue}!`}</p>}
+
+    </div>
+
   )
+
+
 }
 
 export default Personalgreeting
